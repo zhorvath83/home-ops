c8714bcbd568ba5e348e25b13765c6830ba17be6
zhorvath83 <zhorvath83@gmail.com>
2021-08-10 23:25:01 +0200

    recipes database-bootstrap

--- cluster/apps/home/recipes/create-database.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE recipes;
-CREATE USER recipes WITH ENCRYPTED PASSWORD '${SECRET_PSQL_RECIPES_PWD}';
-GRANT all privileges ON database recipes TO recipes;
-ALTER DATABASE recipes OWNER TO recipes;
