4dc462491432301a345df01dcbd5f9693b0b66c8
Horváth Zoltán <zhorvath83@gmail.com>
2022-04-15 13:09:52 +0200

    Flux webhook exclude from UserAuth

--- provision/terraform/cloudflare/cloudflare_access.tf
+++ provision/terraform/cloudflare/cloudflare_access.tf
@@ -18,7 +18,7 @@ resource "cloudflare_access_identity_provider" "pin_login" {
   type       = "onetimepin"
 }
 
-# Oauth
+# Google Oauth
 resource "cloudflare_access_identity_provider" "google_oauth" {
   account_id = data.sops_file.cluster_secrets.data["stringData.SECRET_CF_ACCOUNT_ID"]
   name       = "Sign in with Google account"
@@ -27,26 +27,6 @@ resource "cloudflare_access_identity_provider" "google_oauth" {
   config {
     client_id     = data.sops_file.cluster_secrets.data["stringData.SECRET_CF_ACCESS_GOOGLE_CL_ID"]
     client_secret = data.sops_file.cluster_secrets.data["stringData.SECRET_CF_ACCESS_GOOGLE_CL_SECRET"]
-  }
-}
-
-# Private website
-resource "cloudflare_access_application" "private_website" {
-  zone_id          = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  name             = "Private website"
-  domain           = "www.${data.sops_file.cluster_secrets.data["stringData.SECRET_DOMAIN"]}"
-  type             = "self_hosted"
-}
-
-resource "cloudflare_access_policy" "private_website_bypass_policy" {
-  application_id = cloudflare_access_application.private_website.id
-  zone_id        = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  name           = "Bypass"
-  precedence     = "1"
-  decision       = "bypass"
-
-  include {
-    everyone = true
   }
 }
 
@@ -68,5 +48,45 @@ resource "cloudflare_access_policy" "private_cloud_user_auth_policy" {
 
   include {
     group = [cloudflare_access_group.my_users.id]
+  }
+}
+
+# Private website www exclude from UserAuth
+resource "cloudflare_access_application" "private_website" {
+  zone_id          = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  name             = "Private website"
+  domain           = "www.${data.sops_file.cluster_secrets.data["stringData.SECRET_DOMAIN"]}"
+  type             = "self_hosted"
+}
+
+resource "cloudflare_access_policy" "private_website_bypass_policy" {
+  application_id = cloudflare_access_application.private_website.id
+  zone_id        = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  name           = "Bypass"
+  precedence     = "1"
+  decision       = "bypass"
+
+  include {
+    everyone = true
+  }
+}
+
+# Flux webhook exclude from UserAuth
+resource "cloudflare_access_application" "flux_webhook" {
+  zone_id          = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  name             = "Flux webhook"
+  domain           = "flux-webhook.${data.sops_file.cluster_secrets.data["stringData.SECRET_DOMAIN"]}"
+  type             = "self_hosted"
+}
+
+resource "cloudflare_access_policy" "flux_webhook_bypass_policy" {
+  application_id = cloudflare_access_application.flux_webhook.id
+  zone_id        = lookup(data.cloudflare_zones.domain.zones[0], "id")
+  name           = "Bypass"
+  precedence     = "1"
+  decision       = "bypass"
+
+  include {
+    everyone = true
   }
 }
