0aa42de822b7a1f86853f8ce74bb3b9c6b7ffa2e
zhorvath83 <git@horvathzoltan.me>
2024-03-10 11:29:42 +0100

    linting

--- provision/router/mqttforward.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Place it to /jffs/addons/YazFi.d/userscripts.d
-iptables --insert YazFiFORWARD --in-interface wl0.1 --out-interface br0 --destination 192.168.1.51 --proto tcp --dport 1883 -j ACCEPT
