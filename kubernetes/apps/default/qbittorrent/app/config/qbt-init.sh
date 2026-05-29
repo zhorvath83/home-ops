0b7a16a9e40595858f5c503394fe8562506662b3
Horváth Zoltán <git@horvathzoltan.me>
2026-05-29 22:12:51 +0200

    qbt

--- kubernetes/apps/default/qbittorrent/app/config/qbt-init.sh
+++ kubernetes/apps/default/qbittorrent/app/config/qbt-init.sh
@@ -3,15 +3,14 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# IPFILTER_FILE="/ipfilter/ipfilter.dat"
-# IPFILTER_URL="https://github.com/DavidMoore/ipfilter/releases/download/lists/ipfilter.dat.gz"
-#
-# # --- Download ipfilter ---
-# echo "Downloading ipfilter.dat..."
-# if curl --silent --location --fail "${IPFILTER_URL}" | gunzip > "${IPFILTER_FILE}" 2>/dev/null; then
-#   LINE_COUNT=$(wc -l < "${IPFILTER_FILE}")
-#   echo "ipfilter.dat downloaded (${LINE_COUNT} lines)."
-# else
-#   echo "WARNING: Failed to download ipfilter.dat. qBittorrent will start without IP filtering."
-#   rm -f "${IPFILTER_FILE}"
-# fi
+IPFILTER_FILE="/ipfilter/ipfilter.dat"
+IPFILTER_URL="https://github.com/DavidMoore/ipfilter/releases/download/lists/ipfilter.dat.gz"
+# --- Download ipfilter ---
+echo "Downloading ipfilter.dat..."
+if curl --silent --location --fail "${IPFILTER_URL}" | gunzip > "${IPFILTER_FILE}" 2>/dev/null; then
+  LINE_COUNT=$(wc -l < "${IPFILTER_FILE}")
+  echo "ipfilter.dat downloaded (${LINE_COUNT} lines)."
+else
+  echo "WARNING: Failed to download ipfilter.dat. qBittorrent will start without IP filtering."
+  rm -f "${IPFILTER_FILE}"
+fi
